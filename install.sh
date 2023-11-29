f5619ccba8d5bb7944a0d3e870426af323cf8d19
ren <ren.kan.zen@gmail.com>
2023-11-29 16:48:07 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eu
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt-get install -y build-essential fzf wget zsh
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# create symbolic links
+cd ${HOME}/dotfiles
+
+for file in .??*; do
+    [ ${file} == '.git' ] && continue
+    [ ${file} == '.gitignore' ] && continue
+    ln -fs ${PWD}/${file} ${HOME}
+done
