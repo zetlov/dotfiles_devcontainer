e63b9f4032f68736c2c5f26bb2943ed796a992a3
zetlov <zetlov00@gmail.com>
2024-01-16 18:15:38 +0000

    added git lfs

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ set -eu
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt-get install -y build-essential wget zsh
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get update
+
+sudo apt-get install -y build-essential wget zsh git-lfs
+git lfs install
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
