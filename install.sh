05b849d82fff7732d07c7bc5a8b5df3de8f798cb
zetlov <zetlov00@gmail.com>
2024-04-13 11:33:53 +0900

    fixed install script

--- install.sh
+++ install.sh
@@ -1,26 +1,18 @@
-!/usr/bin sh
+#!/bin/sh
 
 set -eu
 
 sudo apt update -y
 sudo apt upgrade -y
 
-# git lfs
-# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-# sudo apt-get update
-# sudo apt-get install -y git-lfs
-
 sudo apt-get install -y build-essential wget zsh
-# git lfs install
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # create symbolic links
-cd ${HOME}/dotfiles
 
-for file in .??*; do
-    [ ${file} == '.git' ] && continue
-    [ ${file} == '.gitignore' ] && continue
-    ln -fs ${PWD}/${file} ${HOME}
-done
+cd ~/
+
+ln -fs ~/dotfiles/.zshrc .
+ln -fs ~/dotfiles/.gitconfig .
