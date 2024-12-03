46189e3d6ec04ab8a538f4b0dbc970978a954d8f
zetlov <zetlov00@gmail.com>
2024-12-03 21:03:23 +0900

    20241203

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
 
-sudo apt-get install -y build-essential wget
+sudo apt-get install -y build-essential wget zsh
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
