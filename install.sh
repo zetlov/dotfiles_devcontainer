132d3d18d2eedd12f73a772f35a45d24b1f77283
zetlov <zetlov00@gmail.com>
2024-12-03 20:25:07 +0900

    test

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ set -eu
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt-get install -y build-essential wget zsh
+sudo apt-get install -y build-essential wget
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # change default shell
 # if [ "$SHELL" != "$(which zsh)" ]; then
-chsh -s $(which zsh) || echo "Please try running chsh -s $(which zsh)"
+# chsh -s $(which zsh) || echo "Please try running chsh -s $(which zsh)"
+chsh -s "/usr/bin/zsh"
 # fi
 
 # create symbolic links
