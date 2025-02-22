658deb626c526e7a5f6c1964f6b2caa35a88f230
zetlov <zetlov00@gmail.com>
2025-02-22 22:25:20 +0900

    fixed oh-my-zsh install script

--- install.sh
+++ install.sh
@@ -8,7 +8,22 @@ sudo apt upgrade -y
 sudo apt-get install -y build-essential wget zsh
 
 # install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ ! -d ${HOME}/.oh-my-zsh ]; then
+    echo "installing oh-my-zsh..."
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+fi
+
+echo "installing zsh plugins..."
+PLUGINS_DIR=${HOME}/.oh-my-zsh/custom/plugins
+
+# zsh-autosuggestions
+if [ ! -d ${PLUGINS_DIR}/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${PLUGINS_DIR}/zsh-autosuggestions
+fi
+# zsh-syntax-highlighting
+if [ ! -d ${PLUGINS_DIR}/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${PLUGINS_DIR}/zsh-syntax-highlighting
+fi
 
 # change default shell
 # if [ "$SHELL" != "$(which zsh)" ]; then
